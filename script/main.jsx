ba9d43244578e6bb36e1fcfc449d1658f16d3737
Adam Kruczek <Adam.Krucz1@gmail.com>
2017-12-12 21:48:38 +0100

    Change styles and scripts, improved validation

--- script/main.jsx
+++ script/main.jsx
@@ -1,12 +1,17 @@
 import "../style/main.scss";
-const url = "https://my-json-server.typicode.com/akruczek/json-server/users"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const url = "http://localhost:3000/users/";
 
 const buttonLogin = $("#buttonLogin");
 const buttonCreate = $("#buttonCreate");
-const inputLogin = $("#login");
-const inputPassword = $("#password");
-const inputEmail = $("#email");
+const inputLogin = $("#inputLogin");
+const inputPassword = $("#inputPassword");
+const inputEmail = $("#inputEmail");
 const errorMessage = $("#errorMessage");
+const createAccountLink = $("#createAccountLink");
+let isValid;
 
 $(() => {
   console.log("test");
@@ -19,85 +24,96 @@ $(() => {
       dataType: "json"
     })
     .done((response) => {
-      // console.log(response);   //infoline
-      for (let i=0; i<response.length;) {
+      console.log(response);   //infoline
+      for (let i=0; i<response.length; i++) {
         if ((response[i].login === inputLogin.val() || response[i].email === inputLogin.val()) && response[i].password === inputPassword.val()) {
           console.log("poprawny login oraz hasło.");  //infoline
+          inputLogin.addClass("valid"); inputLogin.removeClass("invalid");
+          inputPassword.addClass("valid"); inputPassword.removeClass("invalid");
           errorMessage.text("");
           //PRZEKIEROWANIE DO APLIKACJI
           break;
         }
         else {
-          i++;
-          if (i === response.length) {
+          if (i === response.length-1) {
             console.log("[!] Niepoprawny login lub hasło.");  //infoline
+            inputLogin.addClass("invalid");
+            inputPassword.addClass("invalid");
             errorMessage.text("[!] Niepoprawny login i/lub hasło.");
           }
         }
       }
     })
-    .fail((error) => {
-      console.log(error);
-    });
+    .fail(error => { console.log(error); });
   });
 
-  //TWORZENIE NOWEGO KONTA
-  buttonCreate.on("click", () => {
-    console.log(inputPassword.val().length);
-    if (inputLogin.val().length < 5)  //WALIDACJA LOGINU
-      errorMessage.text("[!] Login musi zawierać przynajmniej 5 znaków.");
+  //ZMIANA KLASY (PODŚWIETLENIE INPUTA)
+  function toggleValid(el, valid) {
+    if (valid) {
+      el.addClass("valid");
+      el.removeClass("invalid"); }
     else {
-      if (inputPassword.val().length < 5)  //WALIDACJA HASŁA
-        errorMessage.text("[!] Hasło musi zawierać przynajmniej 5 znaków.");
-      else {
-        if (!/^[a-zA-Z0-9- ]*$/.test(inputPassword.val()))  //DALSZA WALIDACJA HASŁA
-          errorMessage.text("[!] Hasło może zawierać tylko litery i cyfry.");
-        else {
-          if (inputEmail.val().indexOf("@") === -1 || inputEmail.val().indexOf(".") === -1) //WALIDACJA E-MAIL
-            errorMessage.text("[!] Niepoprawny email");
-          else {  //POMYŚLNE ZAKOŃCZENIE WALIDACJI WSTĘPNEJ
-            errorMessage.text("");
-            $.ajax({
-              url: url,
-              method: "GET",
-              dataType: "json"
-            })
-            .done((response) => {
-              // console.log(response);   //infoline
-              for (let i=0; i<response.length;) {
-                if (response[i].login === inputLogin.val()) { //WALIDACJA NAZWY UŻYTKOWNIKA Z BAZĄ
-                  errorMessage.text("[!] Użytkownik o takim loginie już istnieje.");
-                  break;
-                }
-                else if (response[i].email === inputEmail.val()) {  //WALIDACJA E-MAIL Z BAZĄ
-                  errorMessage.text("[!] Użytkownik o takim adresie e-mail już istnieje.");
-                  break;
-                }
-                else {  //POMYŚLNE ZAKOŃCZENIE WALIDACJI
-                  i++;
-                  if (i === response.length) {
-                    errorMessage.text("");
-                    console.log("Pomyślnie utworzono konto");
-                    createAccount({
-                      login: inputLogin.val(),
-                      password: inputPassword.val(),
-                      email: inputEmail.val(),
-                      id: inputLogin.val()
-                    });
-                    //PRZEKIEROWANIE NA STRONĘ APLIKACJI
-                  }
-                }
-              }
-            })
-            .fail((error) => {
-              console.log(error);
-            });
-          }
-        }
-      }
-    }
-  });
+      el.addClass("invalid");
+      el.removeClass("valid"); }
+  }
 
+  //WALIDACJA LOGINU ORAZ HASŁA
+  function LOGIN_PASS_VALID(element, error) {
+    if (element.val().length < 5 || element.val().length > 18 || !/^[a-zA-Z0-9- ]*$/.test(element.val())) {
+      errorMessage.text(error);
+      toggleValid(element, false);
+      return false; }
+    else {
+      errorMessage.text("");
+      toggleValid(element, true);
+      return true; }
+  }
+
+  //WALIDACJA ADRESU E-MAIL
+  function EMAIL_VALID(element) {
+    if (element.val().length < 6 || element.val().indexOf("@") === -1 || element.val().indexOf(".") === -1) {
+      errorMessage.text("[!] Niepoprawny adres e-mail");
+      toggleValid(element, false);
+      return false; }
+    else {
+      errorMessage.text("");
+      toggleValid(element, true);
+      return true; }
+  }
+
+  //WALIDACJA Z BAZĄ \/
+  function DATABASE_VALID(login, email) {
+    isValid = false;
+    $.ajax({
+      url: url,
+      method: "GET",
+      dataType: "json"
+    })
+    .done((response) => {
+      for (let i = 0; i<response.length; i++) {
+        // console.log(response[i].email);
+        if (response[i].email === email) {
+          errorMessage.text("[!] Użytkownik o takim adresie e-mail już istnieje");
+          toggleValid(inputEmail, false); }
+        else if (response[i].login === login) {
+          errorMessage.text("[!] Użytkownik o takim loginie już istnieje");
+          toggleValid(inputLogin, false); }
+        else {
+          if (i === response.length-1) {
+            errorMessage.text("[!] Pomyślnie utworzono konto");
+            toggleValid(inputLogin, true); toggleValid(inputEmail, true);
+            createAccount({
+              id: inputLogin.val() + inputPassword.val()[0] + inputEmail.val()[inputEmail.val().length-1],
+              login: inputLogin.val(),
+              password: inputPassword.val(),
+              email: inputEmail.val()
+            }); } }
+      }
+    })
+    .fail(error => {console.log(error);});
+  }
+
+  //TWORZENIE NOWEGO UŻYTKOWNIKA
   function createAccount(newUser) {
     console.log(newUser);
     $.ajax({
@@ -106,12 +122,27 @@ $(() => {
       data: newUser
     })
     .done(response => {
+      console.log("Added new user", newUser);
       console.log(response);
     })
     .fail(error => {
       console.log(error);
     });
   }
+
+  //TWORZENIE NOWEGO KONTA
+  buttonCreate.on("click", () => {
+    if (LOGIN_PASS_VALID(inputLogin, "[!] Login musi zawierać od 5 do 18 znaków oraz tylko litery i cyfry.")) {
+      console.log("...login validation correct");
+      if (LOGIN_PASS_VALID(inputPassword, "[!] Hasło musi zawierać od 5 do 18 znaków oraz tylko litery i cyfry.")) {
+        console.log("...password validation correct");
+        if (EMAIL_VALID(inputEmail)) {
+          console.log("...email validation correct");
+          DATABASE_VALID(inputLogin.val(), inputEmail.val());
+        }
+      }
+    }
+  });
 
   //FUNKCJA USUWAJĄCA UŻYTKOWNIKA (PO id)
   function removeAccount(id) {
