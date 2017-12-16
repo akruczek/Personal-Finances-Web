a2a9c21d44d3133433c6257ace59292ad2afdae4
Adam Kruczek <Adam.Krucz1@gmail.com>
2017-12-17 00:00:37 +0100

    App Component - Header, navbar (tooltips)

--- script/app.jsx
+++ script/app.jsx
@@ -1,4 +1,3 @@
-// import "../style/main.scss";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import	{	Router, IndexLink, IndexRoute, hashHistory }	from 'react-router';
@@ -14,9 +13,24 @@ export default class Application extends React.Component {
   }
 
   render() {
-    console.log("Welcome in app " + userObject.login);
-    return (
-      <h1>{userObject.login}</h1>
+    return userObject!==undefined ? (
+      <div className="mainApp">
+        <header className="mainHeader">
+          <nav>
+            <div className="nav-wrapper">
+              <a href="#!" className="brand-logo"><i className="material-icons">adb</i>{userObject.login}</a>
+              <ul className="right hide-on-med-and-down">
+                <li className="tooltip" data-title="tooltip"><a><i className="material-icons">search</i></a></li>
+                <li className="tooltip" data-title="tooltip"><a><i className="material-icons">view_module</i></a></li>
+                <li className="tooltip" data-title="tooltip"><a><i className="material-icons">refresh</i></a></li>
+                <li className="tooltip" data-title="Wyloguj"><a data-title="tooltip" href="/"><i className="material-icons">exit_to_app</i></a></li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+      </div>
+    ) : (
+      <h1 style={{fontSize: "60px", color: "darkred", textAlign: "center"}}>You have to log in!</h1>
     );
   }
 }
